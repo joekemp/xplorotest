acac373e2dc7cd25a7bc978615922fbd349e59ae
Joe Kemp <joe@joekemp.net>
2021-05-15 14:51:49 +0100

    Server now serves frontend

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 import express from "express";
+import path from "path";
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const pwd = path.resolve();
+
+app.use(express.static(path.resolve(pwd, 'client/build')));
+
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+
+app.get('*', (req, res) => {
+  const index = path.resolve(pwd, 'client/build', 'index.html');
+  console.log(`sending file: ${index}`);
+  res.sendFile(index);
 });
 
 app.listen(PORT, () => {
