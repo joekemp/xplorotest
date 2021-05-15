636b0a146167f49e90ac2e6b61ca965059c4b7e2
Joe Kemp <joe@joekemp.net>
2021-05-15 18:14:59 +0100

    Starts work on client - removes unused fluff
     - Logos removed
     - reportWebVitals gone as performance out of scope
     - First test changed to pass

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('contains header with title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Xploro Test/i);
   expect(linkElement).toBeInTheDocument();
 });
