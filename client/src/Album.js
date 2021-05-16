7a098d51503d057190c76119302845f4c51565f0
Joe Kemp <joe@joekemp.net>
2021-05-16 16:42:48 +0100

    Album component now reads props
    Tests now pass.

--- client/src/Album.js
+++ client/src/Album.js
@@ -2,18 +2,31 @@ import React from "react";
 import Card from 'react-bootstrap/Card';
 import SpotifyLinks from "./SpotifyLinks";
 
-export const Album = (props) => (
-  <Card>
-    <Card.Img variant="top" src="https://i.scdn.co/image/ab67616d00001e02e787cffec20aa2a396a61647" />
-    <Card.Body>
-      <div className="float-right">2019</div>
-      <Card.Title>Lover</Card.Title>
-      <SpotifyLinks />
-      <Card.Subtitle>
-        <a href="https://open.spotify.com/artist/06HL4z0CvFAxyc27GXpf02">Taylor Swift</a>
-      </Card.Subtitle>
-    </Card.Body>
-  </Card>
-);
+export const Album = (props) => {
+  if (!props.album) {
+    return <div>Error: No album property passed into Album</div>
+  }
+  const album = props.album;
+  const title = album.name;
+  const releaseYear = album.release_date.substring(0, 4);
+  const image = album.images[1].url;
+  const artist = album.artists[0];
+  const artistName = artist.name;
+  const artistLink = artist.external_urls.spotify;
+
+  return (
+    <Card>
+      <Card.Img variant="top" src={image} />
+      <Card.Body>
+        <div className="float-right">{releaseYear}</div>
+        <Card.Title>{title}</Card.Title>
+        <SpotifyLinks album={album} />
+        <Card.Subtitle>
+          <a href={artistLink}>{artistName}</a>
+        </Card.Subtitle>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default Album;
