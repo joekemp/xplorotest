ecdb6d6cb45694c4455a384fbf55ea4989b98827
Joe Kemp <joe@joekemp.net>
2021-05-16 16:06:45 +0100

    SpotifyLinks implemented to use properties
    Tests now pass.

--- client/src/SpotifyLinks.js
+++ client/src/SpotifyLinks.js
@@ -1,14 +1,26 @@
 import React from "react";
 
-export const SpotifyLinks = (props) => (
-  <div className="float-right">
-    <a href="https://open.spotify.com/album/1NAmidJlEaVgA3MpcPFYGq" title="Open album online" target="_blank" rel="noreferrer">
-      <img src="web.png" alt="web" className="ml-4 mr-1" />
-    </a>
-    <a href="spotify:album:1NAmidJlEaVgA3MpcPFYGq" title="Open album in Spotify">
-      <img src="spotify.png" alt="spotify" className="mr-1 ml-1" />
-    </a>
-  </div>
-);
+export const SpotifyLinks = (props) => {
+  if (!props.album ||
+      !props.album.external_urls ||
+      !props.album.external_urls.spotify ||
+      !props.album.uri) {
+    return <div>Error: No album property passed into SpotifyLinks</div>
+  }
+
+  const webLink = props.album.external_urls.spotify;
+  const spotifyLink = props.album.uri;
+
+  return (
+    <div className="float-right">
+      <a href={webLink} title="Open album online" target="_blank" rel="noreferrer">
+        <img src="web.png" alt="web" className="ml-4 mr-1" />
+      </a>
+      <a href={spotifyLink} title="Open album in Spotify">
+        <img src="spotify.png" alt="spotify" className="mr-1 ml-1" />
+      </a>
+    </div>
+  )
+};
 
 export default SpotifyLinks;
