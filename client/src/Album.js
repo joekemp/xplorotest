dc3070dda4a89993182774c7a7e72b8d81e85052
Joe Kemp <joe@joekemp.net>
2021-05-16 12:32:42 +0100

    Creates Album component
    The idea is that Album renders a single album and Albums iterates over
    the album data, passing it to Album.
    
    Test fails as Album not currently implemented.

---
+++ client/src/Album.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Card from 'react-bootstrap/Card';
+
+export const Album = (props) => (
+  <p>TODO</p>
+);
+
+export default Album;
--- client/src/Albums.js
+++ client/src/Albums.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
+import Album from "./Album.js";
 
 export const Albums = (props) => {
   const [sm, md, lg] = [12, 6, 4];
@@ -12,24 +12,7 @@ export const Albums = (props) => {
       <h3>Albums</h3>
       <Row>
         <Col xs={sm} md={md} lg={lg} className="mb-4">
-          <Card>
-            <Card.Img variant="top" src="https://i.scdn.co/image/ab67616d00001e02e787cffec20aa2a396a61647" />
-            <Card.Body>
-              <div className="float-right">2019</div>
-              <Card.Title>Lover</Card.Title>
-              <div className="float-right">
-                <a href="https://open.spotify.com/album/1NAmidJlEaVgA3MpcPFYGq" title="Open album online" target="_blank" rel="noreferrer">
-                  <img src="web.png" alt="web" className="ml-4 mr-1" />
-                </a>
-                <a href="spotify:album:1NAmidJlEaVgA3MpcPFYGq" title="Open album in Spotify">
-                  <img src="spotify.png" alt="spotify" className="mr-1 ml-1" />
-                </a>
-              </div>
-              <Card.Subtitle>
-                <a href="https://open.spotify.com/artist/06HL4z0CvFAxyc27GXpf02">Taylor Swift</a>
-              </Card.Subtitle>
-            </Card.Body>
-          </Card>
+          <Album />
         </Col>
       </Row>
     </>
