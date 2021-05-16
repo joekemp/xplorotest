de52b811098ce58b609bc52ee892d9309c530e98
Joe Kemp <joe@joekemp.net>
2021-05-16 15:23:33 +0100

    Adds test for SpotifyLinks component
    This now completes testing of the structure of the page in terms of the
    composition of components.
    
    Next step will be to provide new album data to force us to have the
    components render based on the properties passed in.

---
+++ client/src/SpotifyLinks.test.js
@@ -0,0 +1,73 @@
+import React from "react";
+import {shallow} from 'enzyme';
+import SpotifyLinks from "./SpotifyLinks";
+
+const WEB_LINK_TITLE = "Open album online";
+const SPOTIFY_LINK_TITLE = "Open album in Spotify";
+
+describe("Tests SpotifyLinks component", () => {
+
+  it("should have a web link and a Spotify link to the album", () => {
+    const wrapper = shallow(<SpotifyLinks results={albumTaylorSwiftLover} />);
+    const links = wrapper.find("a");
+    expect(links).toHaveLength(2);
+    
+    const webLink = links.at(0);
+    expect(webLink.props().title).toEqual(WEB_LINK_TITLE);
+    expect(webLink.props().href).toEqual("https://open.spotify.com/album/1NAmidJlEaVgA3MpcPFYGq");
+
+    const spotifyLink = links.at(1);
+    expect(spotifyLink.props().title).toEqual(SPOTIFY_LINK_TITLE);
+    expect(spotifyLink.props().href).toEqual("spotify:album:1NAmidJlEaVgA3MpcPFYGq");
+  });
+
+});
+
+
+//
+// Test input data
+//
+
+const albumTaylorSwiftLover = {
+  album_type: "album",
+  artists: [
+    {
+      external_urls: {
+        spotify: "https://open.spotify.com/artist/06HL4z0CvFAxyc27GXpf02",
+      },
+      href: "https://api.spotify.com/v1/artists/06HL4z0CvFAxyc27GXpf02",
+      id: "06HL4z0CvFAxyc27GXpf02",
+      name: "Taylor Swift",
+      type: "artist",
+      uri: "spotify:artist:06HL4z0CvFAxyc27GXpf02",
+    },
+  ],
+  external_urls: {
+    spotify: "https://open.spotify.com/album/1NAmidJlEaVgA3MpcPFYGq",
+  },
+  href: "https://api.spotify.com/v1/albums/1NAmidJlEaVgA3MpcPFYGq",
+  id: "1NAmidJlEaVgA3MpcPFYGq",
+  images: [
+    {
+      height: 640,
+      url: "https://i.scdn.co/image/ab67616d0000b273e787cffec20aa2a396a61647",
+      width: 640,
+    },
+    {
+      height: 300,
+      url: "https://i.scdn.co/image/ab67616d00001e02e787cffec20aa2a396a61647",
+      width: 300,
+    },
+    {
+      height: 64,
+      url: "https://i.scdn.co/image/ab67616d00004851e787cffec20aa2a396a61647",
+      width: 64,
+    },
+  ],
+  name: "Lover",
+  release_date: "2019-08-23",
+  release_date_precision: "day",
+  total_tracks: 18,
+  type: "album",
+  uri: "spotify:album:1NAmidJlEaVgA3MpcPFYGq",
+};
