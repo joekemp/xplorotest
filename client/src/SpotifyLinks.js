2ed79ac8f566eb173f15217f28c839464eac63d5
Joe Kemp <joe@joekemp.net>
2021-05-16 12:36:54 +0100

    SpotifyLinks implemented, tests pass

--- client/src/SpotifyLinks.js
+++ client/src/SpotifyLinks.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 export const SpotifyLinks = (props) => (
-  <p>TODO</p>
+  <div className="float-right">
+    <a href="https://open.spotify.com/album/1NAmidJlEaVgA3MpcPFYGq" title="Open album online" target="_blank" rel="noreferrer">
+      <img src="web.png" alt="web" className="ml-4 mr-1" />
+    </a>
+    <a href="spotify:album:1NAmidJlEaVgA3MpcPFYGq" title="Open album in Spotify">
+      <img src="spotify.png" alt="spotify" className="mr-1 ml-1" />
+    </a>
+  </div>
 );
 
 export default SpotifyLinks;
