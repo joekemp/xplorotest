d43268f1fa36a6c979336911059af38910de20d4
Joe Kemp <joe@joekemp.net>
2021-05-16 12:34:15 +0100

    Album implemented, tests pass

--- client/src/Album.js
+++ client/src/Album.js
@@ -2,7 +2,24 @@ import React from "react";
 import Card from 'react-bootstrap/Card';
 
 export const Album = (props) => (
-  <p>TODO</p>
+  <Card>
+    <Card.Img variant="top" src="https://i.scdn.co/image/ab67616d00001e02e787cffec20aa2a396a61647" />
+    <Card.Body>
+      <div className="float-right">2019</div>
+      <Card.Title>Lover</Card.Title>
+      <div className="float-right">
+        <a href="https://open.spotify.com/album/1NAmidJlEaVgA3MpcPFYGq" title="Open album online" target="_blank" rel="noreferrer">
+          <img src="web.png" alt="web" className="ml-4 mr-1" />
+        </a>
+        <a href="spotify:album:1NAmidJlEaVgA3MpcPFYGq" title="Open album in Spotify">
+          <img src="spotify.png" alt="spotify" className="mr-1 ml-1" />
+        </a>
+      </div>
+      <Card.Subtitle>
+        <a href="https://open.spotify.com/artist/06HL4z0CvFAxyc27GXpf02">Taylor Swift</a>
+      </Card.Subtitle>
+    </Card.Body>
+  </Card>
 );
 
 export default Album;
